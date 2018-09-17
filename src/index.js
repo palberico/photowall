9c57e37b4362fcfc887d71e64170c933587343fb
Pete Alberico <palberico@yahoo.com>
2018-09-16 21:37:16 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/Main.js';
+import './styles/stylesheet.css';
+
+
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
