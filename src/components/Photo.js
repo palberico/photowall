835f20caead11f3aeb94401b3c2fc46990914e2b
Pete Alberico <palberico@yahoo.com>
2018-10-03 21:20:20 -0700

    Single Pic Added

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 function Photo(props) {
 
     const post = props.post
       return (
-        <figure className="figure"> 
-          <img className="photo" src={post.imageLink} alt={post.description} />
+        <figure className="figure">
+          <Link to={'single'}>
+            <img className="photo" src={post.imageLink} alt={post.description} />
+          </Link>       
         <figcaption> <p>{post.description}</p> </figcaption>
         <div className="button-container">
           <button onClick = {() => {
